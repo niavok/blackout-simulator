fdbe73b4a4815e5197dae36b982ccab9121cce2a
Frédéric Bertolus <frederic.bertolus@nerd.nintendo.com>
2020-02-10 13:38:56 +0100

    Fix project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('blackout-simulator2 app is running!');
+    expect(page.getTitleText()).toEqual('blackout-simulator app is running!');
   });
 
   afterEach(async () => {
