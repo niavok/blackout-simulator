ff7ae03d98972491d34740f5551440e957d354ad
Frédéric Bertolus <frederic.bertolus@nerd.nintendo.com>
2020-02-11 13:19:36 +0100

    Navigation prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimulatorComponent } from './simulator/simulator.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
+
+const appRoutes: Routes = [
+  { path: 'simulator', component: SimulatorComponent },
+  { path: 'faq', component: FaqComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimulatorComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
